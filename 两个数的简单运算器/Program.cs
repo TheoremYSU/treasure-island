94766fddbe6bc3ffb8ae3c904745b26d4151b1f3
TheoremYSU <121707571+TheoremYSU@users.noreply.github.com>
2022-12-31 14:26:45 +0800

    Add files via upload

---
+++ 两个数的简单运算器/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace _01_My_First_Demo
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string s, b;
+            int a, c;
+            s = Console.ReadLine();
+            string[] d = s.Split(' ');
+            a = Convert.ToInt32(d[0]);
+            b = (d[1]);
+            c = Convert.ToInt32(d[2]);
+            switch (b)
+            {
+                case "/":
+                    Console.WriteLine(a / c);
+                    break;
+                case "*":
+                    Console.WriteLine(a * c);
+                    break;
+                case "+":
+                    Console.WriteLine(a + c);
+                    break;
+                case "-":
+                    Console.WriteLine(a - c);
+                    break;
+                case "%":
+                    Console.WriteLine(a % c);
+                    break;
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
